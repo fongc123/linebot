c54b8eeeaec6371f21f0b72dabe4b69d16396e16
Fong Chenananporn <fongc123@gmail.com>
2023-08-22 17:53:26 +0700

    test openai

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, request, abort
 import json
 from argparse import ArgumentParser
 import os
-import poe
+import openai
+from copy import deepcopy
 
 from linebot.v3 import (
     WebhookHandler
@@ -27,13 +28,37 @@ from linebot.v3.webhooks import (
 
 CHANNEL_ACCESS_TOKEN = "QQiVYpwcXYAhYrQ0mCDNU8y+iv18MS7PDHoYs4WexlDQ4ZUFtiop0BTVqiWpL+bun9fJfOMgGfdxbeS3oaPzRa7j+zmb6kNcrSBFLkentJ4QPdBjv96OgOPoSUxvRWnetva7nOHqFsRk9am/s2k0kwdB04t89/1O/w1cDnyilFU="
 CHANNEL_SECRET = "c722da9d4e41022ae6906b14b82b9545"
-POE_TOKEN = "sHdaRZeVDSO3bDfPwp0FzA%3D%3D"
+OPENAPI_KEY = "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"
+OPENAI_MESSAGES = {
+    {
+        "role" : "system",
+        "content" : "You are a professional Japanese otaku."
+    },
+    {
+        "role" : "user",
+        "content" : None
+    }
+}
 
 app = Flask(__name__)
 
 configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
-client = poe.PoeClient(POE_TOKEN)
+
+def generate_response(text):
+    openai.api_key = OPENAPI_KEY
+
+    messages = deepcopy(OPENAI_MESSAGES)
+    messages[1]["content"] = text
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0.3,
+        max_tokens=1000,
+    )
+
+    return response
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -55,15 +80,12 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    for chunk in client.send_message("capybara", event.message.text):
-        pass
-    poe_response = chunk["text"]
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=poe_response)]
+                messages=[TextMessage(text=generate_response(event.message.text)['choices'][0]['message']['content'])]
             )
         )
 
